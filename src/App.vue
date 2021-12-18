0f2e0e6753cd5133fa9e5c86ec3bd658d08c8e25
Carlos Henrique da Silva Melo <t-carlos.h.melo@bradesco.com.br>
2021-12-18 13:47:54 -0300

    coisas

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,17 @@
   <v-app>
     <v-container>
       <v-row align="center" justify="center">
-        <v-col cols="24">LOGIN</v-col>
+        <v-file-input
+          chips
+          multiple
+          @change="uploadFile"
+          label="File(s)"
+          v-model="file"
+        ></v-file-input>
+        <!-- <p></p>
+        <p>Supported formats can be found here:</p>
+        <p><a target="_blank" href="https://developer.autodesk.com/en/docs/model-derivative/v2/overview/supported-translations/">https://developer.autodesk.com/en/docs/model-derivative/v2/overview/supported-translations/</a></p>
+     -->
       </v-row>
       <v-row align="center" justify="center">
         <v-col ali cols="6">
@@ -14,17 +24,23 @@
 </template>
 
 <script>
-import ForgeViewer from './components/ForgeViewer';
-
+import ForgeViewer from "./components/ForgeViewer";
+import { uploadFile } from "./components/ForgeViewerServices.js";
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
     ForgeViewer,
   },
 
   data: () => ({
-    //
+    actionString: "/api/forge/datamanagement/bucket/upload",
+    file: null,
   }),
+  methods: {
+    async uploadFile(file) {
+      console.log(file);
+      uploadFile(file)
+    },
+  },
 };
 </script>
--- src/components/ForgeViewer.vue
+++ src/components/ForgeViewer.vue
@@ -1,15 +1,62 @@
 <template>
-  <v-container>
-      <h1>FORGE VIEWER</h1>
+  <v-container :v-if="myToken!= null && urn != null">
+
+    <forge-vuer :getAccessToken="getTokenAsync" :urn="urn" />
   </v-container>
 </template>
 
 <script>
+import ForgeVuer from "forge-vuer";
+import {
+    isAuthenticated,
+  authenticateUser,
+  getToken,
+} from "./ForgeViewerServices.js";
+
 export default {
-    name:"ForgeViewer"
-}
+  name: "ForgeViewer",
+  components: {
+    ForgeVuer,
+  },
+  data: () => {
+    return {
+      myToken: null,
+      urn: null,
+    };
+  },
+  beforeMount() {
+    // var documentId = "urn:" + this.getUrlParameter("urn");
+   var documentId = "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bXktYnVja2V0L215LWF3ZXNvbWUtZm9yZ2UtZmlsZS5ydnQ"
+    console.log("carlos",decodeURIComponent("asdasd"))  
+    console.log(documentId);
+    this.urn = documentId;
+  },
+  methods: {
+    async getTokenAsync(onSuccess) {
+      // An API call to retrieve a valid token should be
+      // done here. A backend service might need to be implemented.
+      if(!isAuthenticated())
+        await authenticateUser();
+      const token = getToken();
+      // For testing purposes, a valid token can be hardcoded but will
+      // last a maximum of 1 hour (3600 seconds.)
+      this.myToken = token;
+      let expireTimeSeconds = 3599;
+      onSuccess(token, expireTimeSeconds);
+    },
+    /**
+     * Get Query string from URL,
+     * we will use this to get the value of 'urn' from URL
+     */
+    getUrlParameter(name) {
+    console.log("carlos",decodeURIComponent("asdasd"))
+      name = name.replace(/[[]/, "\\[").replace(/[\]]/, "\\]");
+      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+      var results = regex.exec(location.search);
+      return results === null
+        ? ""
+        : decodeURIComponent(results[1].replace(/\+/g, " "));
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
